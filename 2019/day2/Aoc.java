b86ac3d7f13a7e1d9ffa4cdd9a3885ccf50b46a8
Juan Rafael García Blanco <juanrgar@Juans-MacBook-Pro.local>
2019-12-31 21:11:52 +0100

    Start with day 2...

---
+++ 2019/day2/Aoc.java
@@ -0,0 +1,60 @@
+
+package day2;
+
+import java.io.FileReader;
+import java.io.StreamTokenizer;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Aoc {
+
+    public static void main(String[] args) {
+	if (args.length != 2) {
+	    Aoc.printUsage();
+	    System.exit(-1);
+	}
+
+	Aoc aoc = new Aoc();
+	aoc.run(args);
+    }
+
+    private static void printUsage() {
+	System.out.println("aoc <1|2> <input_file>");
+    }
+
+    private void run(String[] args) {
+	List<Integer> program = readProgram(args[1]);
+    }
+
+    private List<Integer> readProgram(final String filename) {
+	List<Integer> program = null;
+
+	try {
+	    Reader reader = new FileReader(filename); // throws FileNotFoundException
+	    StreamTokenizer tokenizer = new StreamTokenizer(reader);
+
+	    program = new ArrayList<Integer>();
+
+	    while (tokenizer.nextToken() != StreamTokenizer.TT_EOF) {
+		if (tokenizer.ttype == StreamTokenizer.TT_NUMBER) {
+		    program.add((int) tokenizer.nval);
+		}
+	    }
+
+	    System.out.println("Program size: " + program.size());
+	} catch (FileNotFoundException e) {
+	    System.out.println("File not found: " + filename);
+	    System.exit(-2);
+	} catch (IOException e) {
+	    System.out.println("Error reader line");
+	    System.exit(-3);
+	}
+
+	return program;
+    }
+}
