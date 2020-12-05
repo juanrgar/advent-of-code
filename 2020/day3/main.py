24a76ca047ca4d07c91343b311169d57b4d6c26c
Juan Rafael Garcia Blanco <juanrgar@marvell.com>
2020-12-05 14:32:51 +0100

    update

--- 2020/day3/main.py
+++ 2020/day3/main.py
@@ -2,6 +2,9 @@
 
 import sys
 
+class AreaMap(object):
+    pass
+
 def build_area_map(filename):
     with open(filename, 'r') as f:
         lines = f.readlines()
@@ -14,23 +17,34 @@ def build_area_map(filename):
 
     return (area_map, len(lines[0]), len(lines))
 
-def count_trees(slope):
+def count_trees(area_map, w, h, slope):
     t = 0
     pos = (0, 0)
-    t += visit_pos(pos)
+
+    while pos[1] <= h:
+        t += visit_pos(area_map, pos)
+        pos = next_pos(pos, slope)
+
     return t
 
-def visit_pos(pos, area_map):
+def visit_pos(area_map, pos):
     if get_item(pos) == '#':
         return 1
     return 0
 
-def get_item(pos, area_map):
-    return area_map[pos[1]][pos[0]];
+def get_item(area_map, pos):
+    # TODO
+
+    return area_map[pos[1]][pos[0]]
+
+def next_pos(pos, slope):
+    return (pos[0] + slope[0], pos[1] + slope[1])
 
 def main():
     filename = sys.argv[1]
     (area_map, w, h) = build_area_map(filename)
+    print(w)
+    print(h)
     trees = count_trees((3, -1))
     print(trees)
 
