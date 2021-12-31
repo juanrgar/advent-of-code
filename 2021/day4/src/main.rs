d81fe3336dd27e137c9fe54ef62eca335bec6676
Juan Rafael Garcia Blanco <juanrgar@marvell.com>
2021-12-31 09:50:56 +0100

    2021 day 4: create data structs
    Signed-off-by: Juan Rafael Garcia Blanco <juanrgar@marvell.com>

--- 2021/day4/src/main.rs
+++ 2021/day4/src/main.rs
@@ -2,9 +2,19 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+struct BoardNum {
+    number: u32,
+    checked: bool
+}
+
+struct Board {
+    numbers: Vec<BoardNum>
+}
+
 fn main() {
     let mut numbers: Vec<u32> = Vec::new();
-    let mut boards: Vec<Vec<u32>> = Vec::new();
+    let mut boards: Vec<Board> = Vec::new();
+//    let mut boards: Vec<Vec<u32>> = Vec::new();
 
     if let Ok(mut lines) = read_lines("input.txt") {
         let numbers_line = lines.next().unwrap().unwrap();
@@ -22,12 +32,17 @@ fn main() {
 
             let line = lit.unwrap().unwrap();
             if line.len() == 0 {
-                boards.push(Vec::new());
+                boards.push(Board {
+                    numbers: Vec::new()
+                });
             } else {
                 let board_numbers = line.split_whitespace();
                 for n in board_numbers {
                     let board = boards.last_mut().unwrap();
-                    board.push(n.parse::<u32>().unwrap());
+                    board.numbers.push(BoardNum {
+                        number: n.parse::<u32>().unwrap(),
+                        checked: false
+                    });
                 }
             }
         }
