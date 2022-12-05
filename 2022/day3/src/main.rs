47c19d1519de08afd06bf8d3ca8e156cc5e4f6b6
Juan Rafael Garcia Blanco <juanrgar@gmail.com>
2022-12-05 11:05:17 +0100

    2022 day 3

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -5,10 +5,15 @@ use std::path::Path;
 
 fn find_common_item_type(rucksacks: &Vec<String>) -> char {
     let mut freqs: HashMap<char, u32> = HashMap::new();
+    let mut chars_added: Vec<char> = Vec::new();
 
     for r in rucksacks {
+        chars_added.clear();
         for c in r.chars() {
-            freqs.entry(c).and_modify(|f| *f += 1).or_insert(1);
+            if !chars_added.contains(&c) {
+                freqs.entry(c).and_modify(|f| *f += 1).or_insert(1);
+                chars_added.push(c);
+            }
         }
     }
 
@@ -45,13 +50,13 @@ fn main() {
     let mut priorities: Vec<i32> = Vec::new();
 
     if let Ok(lines) = read_lines("input.txt") {
-        let mut rucksacs: Vec<String> = Vec::new()
+        let mut rucksacs: Vec<String> = Vec::new();
         for line in lines {
             let l = line.unwrap();
-            let parts = l.split_at(l.len() / 2);
-            if rucksacs.len() < 6 {
-                rucksacs.push(vec![parts.0.to_string(), parts.1.to_string()]);
+            if rucksacs.len() < 2 {
+                rucksacs.push(l);
             } else {
+                rucksacs.push(l);
                 let c = find_common_item_type(&rucksacs);
                 let pri;
                 if c.is_lowercase() {
