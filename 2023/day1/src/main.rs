68719e38287e11efee294c3a3d246dc4db618fb6
Juan Rafael Garcia Blanco <juanrgar@gmail.com>
2023-12-01 11:13:08 +0100

    day1 very ugly
    Signed-off-by: Juan Rafael Garcia Blanco <juanrgar@gmail.com>

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -3,25 +3,56 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let mut cur_calories = 0;
-    let mut calories: Vec<i32> = Vec::new();
+    let mut acc = 0;
+
+    let numbers_words = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
     if let Ok(lines) = read_lines("input.txt") {
         for line in lines {
             let l = line.unwrap();
-            if l.len() > 0 {
-                cur_calories += l.parse::<i32>().unwrap();
-            } else {
-                calories.push(cur_calories);
-                cur_calories = 0;
+            println!("{}", l);
+            // let nums: String = l.chars().filter(|c| c.is_digit(10)).collect();
+            //let nums: String = l.chars().filter(|c| c.is_digit(10)).collect();
+            //let num = nums.chars().next().unwrap().to_digit(10).unwrap() * 10 +
+            //    nums.chars().last().unwrap().to_digit(10).unwrap();
+            //acc += num;
+
+            let mut nums: Vec<u32> = Vec::new();
+            for (i, c) in l.char_indices() {
+                if c.is_digit(10) {
+                    nums.push(c.to_digit(10).unwrap());
+                } else {
+                    for w in &numbers_words {
+                        let rem = l.len() - i;
+                        let mut len = w.len();
+                        if len > rem {
+                            len = rem;
+                        }
+                        let s = &l[i..i + len];
+                        if *w == s {
+                            println!("{}", *w);
+                            nums.push(match *w {
+                                "one" => 1,
+                                "two" => 2,
+                                "three" => 3,
+                                "four" => 4,
+                                "five" => 5,
+                                "six" => 6,
+                                "seven" => 7,
+                                "eight" => 8,
+                                "nine" => 9,
+                                _ => 0
+                            });
+                        }
+                    }
+                }
             }
+            println!("{:?}", nums);
+            let num = nums[0] * 10 + nums.last().unwrap();
+            acc += num;
         }
     }
-    calories.sort();
-    calories.reverse();
-    println!("{}", calories[0]);
-    let top_calories = calories[0] + calories[1] + calories[2];
-    println!("{}", top_calories);
+    println!("{}", acc);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
